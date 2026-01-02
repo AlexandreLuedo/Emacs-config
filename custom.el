7a7f55724df599989d84aa6d4cb86cf2db0111e1
Alexandre Luedo <alexandre.luedo@pm.me>
2026-01-02 17:43:30 +0100

    fix: comment titles showed to big
    Just want to display it normally

--- custom.el
+++ custom.el
@@ -20,9 +20,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:weight bold :height 1.4))))
- '(org-level-2 ((t (:weight bold :height 1.15))))
- '(org-level-3 ((t (:weight bold :height 1.1)))))
+ ;; '(org-level-1 ((t (:weight bold :height 1.4))))
+ ;; '(org-level-2 ((t (:weight bold :height 1.15))))
+ ;; '(org-level-3 ((t (:weight bold :height 1.1)))))
 
 (require 'ox-latex)
 (unless (boundp 'org-latex-classes)
